f45f11f226c45681d76ede79ba931eefd40ec107
Serious Zainab <85008588+zainab-oss@users.noreply.github.com>
2022-06-04 19:12:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function capture()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_img" src = "'+data_uri+'"/>';
+    });
+}
+
+console.log ("ml5 Version: ", ml5.version);
+
+classifier = ml5.imageclassifier('', modelLoaded);
