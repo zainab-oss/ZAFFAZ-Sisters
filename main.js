d0542380f2794f170e3ee9be87f63decbabc765e
Serious Zainab <85008588+zainab-oss@users.noreply.github.com>
2022-06-16 17:43:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,4 +18,26 @@ function capture()
 
 console.log ("ml5 Version: ", ml5.version);
 
-classifier = ml5.imageclassifier('', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EInJ40I6e/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+}
+
+function check()
+{
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_name").innerHTML = results[0].label;
+        document.getElementById("result_name_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
